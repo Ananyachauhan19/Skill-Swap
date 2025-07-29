f40cb1972a740f48bcfce7fcd7156440b56a9acb
Anubhav-dhyani <adhyani1105@gmail.com>
2025-07-29 20:14:53 +0530

    ghuhbvub

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -101,7 +101,7 @@ exports.verifyOtp = async (req, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'none',
+    sameSite: 'lax',
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
 
