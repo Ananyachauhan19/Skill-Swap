95106015641a5a13e857b152eea0bca40adaf67b
vssemwal2004 <422semwalvivek@gmail.com>
2025-08-15 22:02:10 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -574,7 +574,8 @@ const Navbar = () => {
                 <Notifications
                   notifications={notifications}
                   setNotifications={setNotifications}
-                  iconSize="w-5 h-5 sm:w-6 sm:h-6"
+                  iconSize="w-5 h-5 sm:w-6 sm:h-6 md:w-5 md:h-5"
+                  className="relative flex items-center justify-center"
                 />
 
                 {/* Profile */}
